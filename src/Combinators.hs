4a0f2ceaebb4473c488df3610a43f3e8b7490700
Pavel Gromov <pagrom27@gmail.com>
2020-03-16 15:10:14 +0300

    Initial from HW03

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Combinators where
 
-import           AST                 (AST (..), Operator (..))
-import           Control.Applicative (Alternative (..))
-import           Text.Printf         (printf)
+import AST                 (AST (..), Operator (..))
+import Control.Applicative (Alternative (..))
+import Text.Printf         (printf)
+
 
 data Result error input result
   = Success input result
@@ -12,52 +15,72 @@ data Result error input result
 newtype Parser error input result
   = Parser { runParser :: input -> Result error input result}
 
-instance Functor (Parser error input) where
-  fmap = error "fmap not implemented"
+instance (Monoid error, Read error) => Functor (Parser error input) where
+  -- В случае успешного разбора модифицирует результат при помощи функции f
+  -- fmap :: (a -> b) -> Parser e i a -> Parser e i b
+  fmap f p = Parser $ \input ->
+        case runParser p input of
+          Success input' result -> Success input' (f result)
+          Failure err   -> Failure err
 
-instance Applicative (Parser error input) where
-  pure = error "pure not implemented"
-  (<*>) = error "<*> not implemented"
+instance (Monoid error, Read error) => Applicative (Parser error input) where
+  pure = return
+  p <*> q = Parser $ \input ->
+    case runParser p input of
+      Success input' f ->
+        case runParser q input' of
+          Success input'' a -> Success input'' (f a)
+          Failure err -> Failure err
+      Failure err -> Failure err
 
-instance Monad (Parser error input) where
-  return = error "return not implemented"
+instance (Monoid error, Read error) => Monad (Parser error input) where
+  -- Всегда завершается успехом, вход не читает, возвращает данное значение
+  return res = Parser $ \input -> Success input res
 
-  (>>=) = error ">>= not implemented"
+  -- Последовательное применение парсеров:
+  -- если первый парсер успешно принимает префикс строки, второй запускается на суффиксе.
+  -- Второй парсер использует результат первого.
+  p >>= x = Parser $ \input ->
+    case runParser p input of
+      Success input' y -> runParser (x y) input'
+      Failure e -> Failure e
 
-instance Monoid error => Alternative (Parser error input) where
-  empty = error "empty not implemented"
+  -- Всегда завершается ошибкой
+  fail e = Parser (\_ -> Failure $ read e)
 
-  (<|>) = error "<|> not implemented"
+instance (Monoid error, Read error) => Alternative (Parser error input) where
+  empty = Parser (\_ -> Failure mempty)
+
+  -- Альтернатива: в случае неудачи разбора первым парсером, парсит вторым
+  p <|> x = Parser $ \input ->
+    case runParser p input of 
+      Failure _ -> runParser x input
+      result    -> result
 
 -- Принимает последовательность элементов, разделенных разделителем
 -- Первый аргумент -- парсер для разделителя
 -- Второй аргумент -- парсер для элемента
 -- В последовательности должен быть хотя бы один элемент
-sepBy1 :: Parser e i sep -> Parser e i a -> Parser e i [a]
-sepBy1 sep elem = error "sepBy1 not implemented"
+sepBy1 :: (Monoid e, Read e) => Parser e i sep -> Parser e i a -> Parser e i [a]
+sepBy1 sep elem = (:) <$> elem <*> many' (sep *> elem)
 
--- Альтернатива: в случае неудачи разбора первым парсером, парсит вторым
-alt :: Parser e i a -> Parser e i a -> Parser e i a
-alt p q = Parser $ \input ->
-  case runParser p input of
-    Failure _ -> runParser q input
-    x         -> x
+sepBy1' :: (Monoid e, Read e) => Parser e i t1 -> Parser e i t -> Parser e i (t, [(t1, t)])
+sepBy1' sep elem = do
+  value <- elem
+  values <- many' (flip fmap elem . (,) =<< sep) <|> return []
+  return (value, values)
 
--- Последовательное применение парсеров:
--- если первый парсер успешно принимает префикс строки, второй запускается на суффиксе.
--- Второй парсер использует результат первого.
-seq' :: Parser e i a -> (a -> Parser e i b) -> Parser e i b
-seq' p f = Parser $ \input ->
-  case runParser p input of
-    Success i a -> runParser (f a) i
-    Failure e   -> Failure e
+sepBy1'' :: (Monoid e, Read e) => Parser e i t1 -> Parser e i t -> Parser e i (t, [(t1, t)])
+sepBy1'' sep elem = do
+    values <- many (flip fmap sep . (,) =<< elem) <|> return []
+    value <- elem
+    return (value, fmap (uncurry $ flip (,)) values)
 
 -- Проверяет, что первый элемент входной последовательности удовлетворяет предикату
 satisfy :: (a -> Bool) -> Parser String [a] a
-satisfy p = Parser $ \input ->
-  case input of
-    (x:xs) | p x -> Success xs x
-    input        -> Failure $ "Predicate failed"
+satisfy p = Parser $ \case
+    (x : xs) | p x -> Success xs x
+    input -> Failure "Predicate failed"
 
 -- Успешно завершается, если последовательность содержит как минимум один элемент
 elem' :: Parser String [a] a
@@ -67,26 +90,12 @@ elem' = satisfy (const True)
 symbol :: (Eq a) => a -> Parser String [a] a
 symbol c = satisfy (==c)
 
--- В случае успешного разбора модифицирует результат при помощи функции f
-map' :: (a -> b) -> Parser e i a -> Parser e i b
-map' f p = Parser $ \input ->
-  case runParser p input of
-    Success i a -> Success i (f a)
-    Failure e   -> Failure e
-
--- Всегда завершается ошибкой
-fail' :: e -> Parser e i a
-fail' e = Parser $ \input -> Failure e
-
--- Всегда завершается успехом, вход не читает, возвращает данное значение
-return' :: a -> Parser e i a
-return' x = Parser $ \input -> Success input x
-
 -- Последовательное применение одного и того же парсера 0 или более раз
-many' :: Parser e i a -> Parser e i [a]
-many' p = some' p `alt` return' []
+many' :: (Monoid e, Read e) =>  Parser e i a -> Parser e i [a]
+many' p = some' p <|> return []
 
 -- Последовательное применения одного и того же парсера 1 или более раз
-some' :: Parser e i a -> Parser e i [a]
-some' p = p `seq'` \r -> map' (r:) (some' p `alt` return' [])
-
+some' :: (Monoid e, Read e) => Parser e i a -> Parser e i [a]
+some' p = do 
+  res <- p
+  (res:) <$> (some' p <|> return [])
